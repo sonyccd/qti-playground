ce4a0f0b1edca555dc37ace0d2ef18c0adcf8b2b
Brad Bazemore <bankersalgo@gmail.com>
2025-08-09 21:33:24 -0400

    refactor: Consolidate UI components and simplify state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 // Removed unused TanStack Query
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
@@ -26,7 +25,6 @@ const App = () => (
   <TooltipProvider>
       <div className="min-h-screen flex flex-col">
         <Toaster />
-        <Sonner />
         <BrowserRouter>
           <AppLayout>
             <div className="flex-1">
